6b88026332be16dec85b26c679ec84d8163c40ca
kaviarasu758 <kaviarasurp758@gmail.com>
2024-12-30 13:59:14 +0530

    Delete callback.js

--- callback.js
+++
@@ -1,9 +0,0 @@
-//callback
-function processUserInput(callback) {
-      const name = "vijay";
-      callback(name);
-    }
-    function greetUser(name) {
-      console.log(`Hello, ${name}! Welcome to our website.`);
-    }
-    processUserInput(greetUser);
